a66f646660fc593fd9a4d0420a89497ded8308cb
Darius <darius.joaquin.reyes@cryptex.ph>
2026-05-15 10:11:46 +0800

    template

---
+++ app/config/theme.ts
@@ -0,0 +1,12 @@
+'use client';
+
+import { Button, createTheme, Text } from '@mantine/core';
+import { Inter } from 'next/font/google';
+
+// const inter = Inter({
+//   subsets: ['latin'],
+//   weight: ['100', '200', '300', '400', '500', '600', '700'],
+// });
+export const theme = createTheme({
+
+});
